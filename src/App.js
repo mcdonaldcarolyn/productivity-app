5d78e228960472f762e4e6b5525a5b6a8d364f75
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2021-10-04 16:26:05 -0400

    first commit, new name

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Let's make a producitivity app
+        </p>``
         <a
           className="App-link"
           href="https://reactjs.org"
