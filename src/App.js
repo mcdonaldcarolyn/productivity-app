71a1a09b7b22435ff179c7d1fc592a052f794d77
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2021-10-13 12:20:48 -0400

    add header and timer components'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import TodoCard from './Components/TodoCard';
+import { Header } from './Components/Header';
+import { Timer } from './Components/Timer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+        <Header/>
+        <TodoCard />
+        <Timer/>
         <p>
           Let's make a producitivity app super fun
         </p>``
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      
     </div>
   );
 }
---
+++ src/Components/TodoCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+function TodoCard() {
+  return (
+    <p>
+      this is the todo card
+    </p>
+
+  );
+
+  
+};
+
+export default TodoCard;
